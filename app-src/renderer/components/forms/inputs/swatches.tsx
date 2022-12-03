313d58fe0338d6a7b9e583c0f14f95afa9d7937c
xi72yow <65042627+xi72yow@users.noreply.github.com>
2022-12-03 01:07:57 +0100

    fix(app): swatches random, hide color chooser on click

--- app-src/renderer/components/forms/inputs/swatches.tsx
+++ app-src/renderer/components/forms/inputs/swatches.tsx
@@ -1,30 +1,30 @@
 import {
-  useMantineTheme,
-  Group,
-  ColorSwatch,
-  Popover,
   ActionIcon,
-  ColorPicker,
-  Grid,
   Button,
+  ColorPicker,
+  ColorSwatch,
+  Grid,
+  Group,
+  Popover,
   Text,
+  useMantineTheme,
 } from "@mantine/core";
+import { UseFormReturnType } from "@mantine/form";
 import { showNotification } from "@mantine/notifications";
-import { IconSquareX, IconEdit } from "@tabler/icons";
+import { IconEdit, IconSquareX, IconWand } from "@tabler/icons";
 import React, { useState } from "react";
+import { random, randomColor } from "screenchaser-core";
+import { ConfigInterface } from "../../database/db";
 
 type Props = {
   label: string;
   defaultValue?: Array<string>;
-  form?: any;
+  form: UseFormReturnType<ConfigInterface>;
   path?: string;
 };
 
-const bubbleColors = ["#24D024", "#EA0D0D"];
-
 export default function Swatches({ label, form, defaultValue, path }: Props) {
   const theme = useMantineTheme();
-  /*   const [checked, setChecked] = useState([]);*/
   const [swatches, setSwatches] = useState(defaultValue);
   const [opened, setOpened] = useState(false);
   const [color, setColor] = useState("#9B03FF");
@@ -39,6 +39,14 @@ export default function Swatches({ label, form, defaultValue, path }: Props) {
       form.setFieldValue(path, swatches);
     }
   }, [swatches]);
+
+  const onClick = (event) => {
+    if (typeof event.target?.className !== "string") return;
+    if (event.target?.className?.toLowerCase().includes("appshell")) {
+      setOpened(false);
+      window.removeEventListener("click", onClick);
+    }
+  };
 
   return (
     <React.Fragment>
@@ -70,83 +78,91 @@ export default function Swatches({ label, form, defaultValue, path }: Props) {
             <ColorSwatch
               key={index}
               onClick={() => {
-                /*     if (!opened)
-              setChecked((c) => {
-                c[index] = !c[index];
-                return [...c];
-              }); */
                 setColor(color);
                 setOpened((open) => true);
               }}
               sx={{ color: "#fff", cursor: "pointer" }}
               color={color}
-            >
-              {/*  {checked[index] ? <CheckIcon width={10} /> : ""} */}
-            </ColorSwatch>
+            ></ColorSwatch>
           ))}
         </Group>
-        <Popover
-          opened={opened}
-          /* onChange={setOpened} */
-          width={300}
-          position="bottom-end"
-          withArrow
-          shadow="md"
-        >
-          <Popover.Target>
-            <ActionIcon
-              onClick={() => {
-                setOpened((open) => !open);
-              }}
-            >
-              {opened ? (
-                <IconSquareX size={18} stroke={1.5} />
-              ) : (
-                <IconEdit size={18} stroke={1.5} />
-              )}
-            </ActionIcon>
-          </Popover.Target>
-          <Popover.Dropdown>
-            <ColorPicker value={color} onChange={setColor} fullWidth />
-            <Grid mt={3}>
-              <Grid.Col span={6}>
-                <Button
-                  onClick={() => {
-                    const swatchesCopy = [...swatches];
-                    if (swatchesCopy.indexOf(color) > -1) {
-                      swatchesCopy.splice(swatchesCopy.indexOf(color), 1);
-                      setSwatches(swatchesCopy);
-                    }
-                  }}
-                  fullWidth
-                >
-                  Remove
-                </Button>
-              </Grid.Col>
-              <Grid.Col span={6}>
-                <Button
-                  onClick={() => {
-                    if (swatches.length >= 7) {
-                      showNotification({
-                        title: "Config Error",
-                        message:
-                          "You have reached the maximum amount of colors",
-                      });
-                      return;
-                    }
-                    if (swatches.includes(color)) {
-                      return;
-                    }
-                    setSwatches((swatches: string[]) => [...swatches, color]);
-                  }}
-                  fullWidth
-                >
-                  Add
-                </Button>
-              </Grid.Col>
-            </Grid>
-          </Popover.Dropdown>
-        </Popover>
+        <Group>
+          <ActionIcon
+            onClick={() => {
+              const randomColors = [];
+              randomColors.push(randomColor());
+              for (let i = 0; i < random(4); i++) {
+                randomColors.push(randomColor());
+              }
+              setSwatches(randomColors);
+            }}
+          >
+            <IconWand size={18} stroke={1.5} />
+          </ActionIcon>
+          <Popover
+            opened={opened}
+            /* onChange={setOpened} */
+            width={300}
+            position="bottom-end"
+            withArrow
+            shadow="md"
+          >
+            <Popover.Target>
+              <ActionIcon
+                onClick={() => {
+                  setOpened((open) => !open);
+                  window.addEventListener("click", onClick);
+                }}
+              >
+                {opened ? (
+                  <IconSquareX size={18} stroke={1.5} />
+                ) : (
+                  <IconEdit size={18} stroke={1.5} />
+                )}
+              </ActionIcon>
+            </Popover.Target>
+            <Popover.Dropdown>
+              <ColorPicker value={color} onChange={setColor} fullWidth />
+              <Grid mt={3}>
+                <Grid.Col span={6}>
+                  <Button
+                    onClick={() => {
+                      const swatchesCopy = [...swatches];
+                      if (swatchesCopy.indexOf(color) > -1) {
+                        swatchesCopy.splice(swatchesCopy.indexOf(color), 1);
+                        setSwatches(swatchesCopy);
+                      }
+                    }}
+                    fullWidth
+                  >
+                    Remove
+                  </Button>
+                </Grid.Col>
+                <Grid.Col span={6}>
+                  <Button
+                    onClick={() => {
+                      if (swatches.length >= 7) {
+                        showNotification({
+                          title: "Config Error",
+                          message:
+                            "You have reached the maximum amount of colors",
+                        });
+                        return;
+                      }
+                      if (swatches.includes(color)) {
+                        return;
+                      }
+                      setSwatches((swatches: string[]) => [...swatches, color]);
+                    }}
+                    fullWidth
+                  >
+                    Add
+                  </Button>
+                </Grid.Col>
+              </Grid>
+            </Popover.Dropdown>
+          </Popover>
+        </Group>
       </Group>
     </React.Fragment>
   );
