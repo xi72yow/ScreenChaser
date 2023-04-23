bf242f1bf3da4a2c90e69e8aae77f97730fad09a
xi72yow <65042627+xi72yow@users.noreply.github.com>
2023-04-23 21:56:36 +0200

    chore(firmware): wifi connection, udp tests

--- firmware-src/main/firmware_main.c
+++ firmware-src/main/firmware_main.c
@@ -1,13 +1,152 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include "sdkconfig.h"
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#include "driver/uart.h"
+#include "driver/gpio.h"
+
 #include "esp_chip_info.h"
 #include "esp_flash.h"
+#include "esp_system.h"
 #include "esp_wifi.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "esp_log.h"
 
-void app_main(void)
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "lwip/sockets.h"
+#include <lwip/netdb.h>
+
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+#define ESP_MAXIMUM_RETRY 10
+#define PORT 4210
+
+static EventGroupHandle_t s_wifi_event_group;
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+static const char *WIFI_TAG = "chaser-wifi";
+
+static const char *UART_TAG = "chaser-serial";
+
+static const char *SOCKET_TAG = "chaser-socket";
+
+static int s_retry_num = 0;
+
+wifi_mode_t wifi_mode;
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < ESP_MAXIMUM_RETRY)
+        {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(WIFI_TAG, "retry to connect to the AP");
+        }
+        else
+        {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(WIFI_TAG, "connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(WIFI_TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = SECRET_SSID,
+            .password = SECRET_PASS,
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(WIFI_TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT)
+    {
+        ESP_LOGI(WIFI_TAG, "connected to ap SSID:%s password:%s",
+                 SECRET_SSID, SECRET_PASS);
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
+        ESP_LOGI(WIFI_TAG, "Failed to connect to SSID:%s, password:%s",
+                 SECRET_SSID, SECRET_PASS);
+    }
+    else
+    {
+        ESP_LOGE(WIFI_TAG, "UNEXPECTED EVENT");
+    }
+
+    /* The event will not be processed after unregister */
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
+}
+
+void print_chip_info(void)
 {
     printf("ScreenChaser Firmware\n");
 
@@ -35,13 +174,100 @@ void app_main(void)
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
     printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+}
 
-    for (int i = 10; i >= 0; i--)
+void keep_wifi_alive(void *pvParameters)
+{
+    while (1)
     {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+
+        esp_netif_ip_info_t ip_info;
+        esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"), &ip_info);
+
+        if (ip_info.ip.addr != 0)
+        {
+            ESP_LOGI(WIFI_TAG, "WiFi is alive");
+        }
+        else
+        {
+            ESP_LOGI(WIFI_TAG, "WiFi is dead");
+            ESP_ERROR_CHECK(esp_wifi_connect());
+        }
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+}
+
+void process_udp_packet()
+{
+    char rx_buffer[128];
+    char addr_str[128];
+    struct sockaddr_in6 dest_addr;
+    int ip_protocol = 0;
+
+    struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
+    dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
+    dest_addr_ip4->sin_family = AF_INET;
+    dest_addr_ip4->sin_port = htons(PORT);
+
+    int sock = socket(AF_INET, SOCK_DGRAM, ip_protocol);
+    if (sock < 0)
+    {
+        ESP_LOGE(SOCKET_TAG, "Unable to create socket: errno %d", errno);
+    }
+    ESP_LOGI(SOCKET_TAG, "Socket created");
+
+    int err = bind(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    if (err < 0)
+    {
+        ESP_LOGE(SOCKET_TAG, "Socket unable to bind: errno %d", errno);
+    }
+    ESP_LOGI(SOCKET_TAG, "Socket bound, port %d", PORT);
+
+    while (1)
+    {
+
+        ESP_LOGI(SOCKET_TAG, "Waiting for data");
+        struct sockaddr_in source_addr;
+        socklen_t socklen = sizeof(source_addr);
+        int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
+
+        // Error occurred during receiving
+        if (len < 0)
+        {
+            ESP_LOGE(SOCKET_TAG, "recvfrom failed: errno %d", errno);
+            break;
+        }
+        // Data received
+        else
+        {
+
+            inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
+
+            rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string...
+            ESP_LOGI(SOCKET_TAG, "Received %d bytes from %s:", len, addr_str);
+            ESP_LOGI(SOCKET_TAG, "%s", rx_buffer);
+        }
+    }
+}
+
+void app_main(void)
+{
+    print_chip_info();
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(WIFI_TAG, "ESP_WIFI_MODE_STA");
+
+    wifi_init_sta();
+
+    xTaskCreate(&keep_wifi_alive, "keep_wifi_alive", 2048, NULL, 10, NULL);
+
+    xTaskCreate(&process_udp_packet, "process_udp_packet", 4096, NULL, 5, NULL);
 }
