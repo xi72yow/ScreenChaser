c19d641112b81cc32eeaefeff1cb1a6cacd4ffdd
xi72yow <65042627+xi72yow@users.noreply.github.com>
2023-04-25 22:09:45 +0200

    chore(app): update dashboard-data

--- app-src/main/helpers/stat-calculator.ts
+++ app-src/main/helpers/stat-calculator.ts
@@ -9,44 +9,36 @@ class StatCalculator {
   }
 
   calculateStats() {
-    this.Manager.chasers.forEach(
-      ({ config, device, emitter, runningEffect }) => {
-        const effect = runningEffect;
-        const data = emitter.getHealth();
+    this.Manager.chasers.forEach(({ config, device, emitter }) => {
+      const data = emitter.getHealth();
 
-        const lastStatisic = this.statistics.get(device.id);
+      const lastStatisic = this.statistics.get(device.id);
 
-        const statisic = {
-          title: emitter.getIp(),
-          task: effect
-            ? effect.getIdentifier()
-            : config.taskCode === "videoChaser"
-            ? "chaser"
-            : null,
-          details: [
-            {
-              title: "Power:",
-              value: data.power,
-              maxPower: data.maxPower,
-              icon: "bolt",
-              diff: lastStatisic
-                ? (data.power / lastStatisic.details[0]?.value) * 100 - 100
-                : 0,
-            },
-            {
-              title: "Package Loss:",
-              value: data.packageloss,
-              icon: "package",
-              diff: lastStatisic
-                ? (data.packageloss / lastStatisic.details[1]?.value) * 100 -
-                  100
-                : 0,
-            },
-          ],
-        };
-        this.statistics.set(device.id, statisic);
-      }
-    );
+      const statisic = {
+        title: device.name || device.ip,
+        task: config.taskCode,
+        details: [
+          {
+            title: "Power:",
+            value: data.power,
+            maxPower: data.maxPower,
+            icon: "bolt",
+            diff: lastStatisic
+              ? (data.power / lastStatisic.details[0]?.value) * 100 - 100
+              : 0,
+          },
+          {
+            title: "Package Loss:",
+            value: data.packageloss,
+            icon: "package",
+            diff: lastStatisic
+              ? (data.packageloss / lastStatisic.details[1]?.value) * 100 - 100
+              : 0,
+          },
+        ],
+      };
+      this.statistics.set(device.id, statisic);
+    });
 
     const arr = Array.from(this.statistics, function (entry) {
       return { ...entry[1] };
